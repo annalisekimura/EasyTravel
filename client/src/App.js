62762901a51abf1b4288ad9d295eb70eefb6696b
annalisekimura <kimuraannalise@gmail.com>
2025-06-24 23:36:22 -0400

    outline of app

---
+++ client/src/App.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import './css/App.css'
+
+function App() {
+
+  const [data, setData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/contents").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+
+  return (
+    
+    <BrowserRouter>
+        <Routes>
+            <Route path="/"
+            element={
+              <div className="home-container">
+                <div className="plane-container">
+                  <img src="/plane.png" alt="Plane" className="plane-image" />
+                </div>
+                <h1 className="home-title">PlanPilot
+                  <div className="arrow-line">
+                    <span className="arrow-head">{">"}</span>
+                  </div>
+                </h1>
+            
+                <div className="home-buttons">
+                  <Link to="/login">
+                    <button className="button green">Login</button>
+                  </Link>
+                  <Link to="/register">
+                    <button className="button blue">Register</button>
+                  </Link>
+                </div>
+              </div>
+            } />
+
+
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+
+
+        </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+const buttonStyle = (bgColor) => ({
+  padding: '12px 24px',
+  fontSize: '16px',
+  backgroundColor: bgColor,
+  color: 'white',
+  border: 'none',
+  borderRadius: '8px',
+  cursor: 'pointer',
+  minWidth: '100px',
+  transition: 'background-color 0.3s ease',
+})
+
+export default App
